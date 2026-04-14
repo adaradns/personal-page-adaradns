cefd51f3af581c79b2d200698b7fb51116658d5b
adaradenis <adaradenis@gmail.com>
2026-04-14 20:06:29 -0300

    se quita la seccion proyectos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home({ dark, onToggleTheme }) {
     <MainLayout dark={dark} onToggleTheme={onToggleTheme}>
       <Hero />
       <About />
-      <Projects />
+      {/* <Projects /> */}
       <Skills />
       <Contact />
     </MainLayout>
